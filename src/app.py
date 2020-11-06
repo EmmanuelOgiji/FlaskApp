3a4602e54883d948e04855d8ef8375b915547c29
emmanuel.pius-ogiji <epiusogiji@gmail.com>
2020-11-06 15:23:39 +0000

    - Add simple functionality to return upper case of word entered in home page text box

--- src/app.py
+++ src/app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+"""
+As a user of this app,
+when I enter the name of a spotify playlist
+and click submit
+then
+"""
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def my_form():
+    return render_template('home.html')
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 
 if __name__ == '__main__':
