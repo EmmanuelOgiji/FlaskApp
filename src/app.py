6af8c93fb5ebe6f3ee25103fdbf9f776c041b947
emmanuel.pius-ogiji <epiusogiji@gmail.com>
2020-11-11 10:49:57 +0000

    - Add spotify authentication - Can now get playlist details via user id

--- src/app.py
+++ src/app.py
@@ -4,22 +4,60 @@ when I enter the name of a spotify playlist
 and click submit
 then
 """
+import base64
+import json
 
+import requests
 from flask import Flask, render_template, request
+
+from src.data_objects.playlist import Playlist
 
 app = Flask(__name__)
 
+clientId = "b5e1ecfbd8fc43d49129ae96eafce387"
+clientSecret = "c142e97219c94c5991d0e8ff1031c0ee"
+
 
 @app.route('/')
-def my_form():
-    return render_template('home.html')
+def user_form():
+    return render_template("user_home.html")
+
+
+def encode_to_base64(message):
+    messagebytes = message.encode('ascii')
+    base64bytes = base64.b64encode(messagebytes)
+    base64message = base64bytes.decode('ascii')
+    return base64message
+
+
+def authenticate_spotify():
+    message = encode_to_base64(f"{clientId}:{clientSecret}")
+    response = requests.post(
+        url="https://accounts.spotify.com/api/token",
+        headers=dict(
+            Authorization=f"Basic {message}"
+        ),
+        data=dict(grant_type="client_credentials")
+
+    )
+
+    token = response.json()['access_token']
+    return token
 
 
 @app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+def list_users_playlists():
+    token = authenticate_spotify()
+    user_id = request.form['user_id']
+
+    response = (requests.get(
+        url=f"https://api.spotify.com/v1/users/{user_id}/playlists",
+        headers=dict(Authorization=f"Bearer {token}")
+    )).json()
+    playlists = []
+    for playlist in response.get("items"):
+        playlists.append(Playlist(playlist).print_details())
+    return json.dumps(playlists)
 
 
 if __name__ == '__main__':
---
+++ src/data_objects/playlist.py
@@ -0,0 +1,13 @@
+class Playlist:
+    def __init__(self, playlist_dict):
+        self.name = playlist_dict.get("name")
+        self.id = playlist_dict.get("id")
+        self.track_details = playlist_dict.get("tracks")
+
+    def print_details(self):
+        return dict(
+            PlaylistName=self.name,
+            PlaylistId=self.id,
+            TrackCount=self.track_details.get("total"),
+            Tracklink=self.track_details.get("href")
+        )
