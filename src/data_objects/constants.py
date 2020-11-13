391b2a22c17ba11000aa6b8670bbc1c1e2cc94d8
emmanuel.pius-ogiji <epiusogiji@gmail.com>
2020-11-13 16:05:07 +0000

    Remove hardcoded credentials

--- src/data_objects/constants.py
+++ src/data_objects/constants.py
@@ -1,3 +1,5 @@
-CLIENT_ID = "b5e1ecfbd8fc43d49129ae96eafce387"
-CLIENT_SECRET = "c142e97219c94c5991d0e8ff1031c0ee"
+import os
+
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 log_level = "DEBUG"
